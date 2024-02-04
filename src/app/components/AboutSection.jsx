81bf1df04c1a4f05b3d819a2e2d50151ac38406d
Yarikama <amaki30226@gmail.com>
2024-02-04 18:12:55 +0800

    add topbar and aboutsection

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,98 @@
+"use client"
+import React, {useTransition, useState} from 'react'
+import Image from 'next/image'
+import TapButton from './TapButton'
+
+const TAB_DATA = [
+    {
+        title: '學歷',
+        id: '學歷',
+        image: '/images/學歷.png',
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>市立高雄中學畢業</li>
+                <li>國立陽明交通大學 工業工程與管理學系</li>
+                <li>輔系 資訊工程學系</li>
+                <li>三次書卷獎</li>
+                <li>GPA 4.05/4.30 Over 151 Credits</li>
+                <li>Last 60 GPA 4.15/4.30</li>
+            </ul>
+        )
+    },{
+        title: '技能',
+        id: '技能',
+        image: '/images/技能.png',
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>React.js</li>
+                <li>Next.js</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>SQL</li>
+                <li>MongoDB</li>
+                <li>Python</li>
+            </ul>
+        )
+    },{
+        title: '經驗',
+        id: '經驗',
+        image: '/images/經驗.png',
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>工業工程與管理學系系學會會長</li>
+                <li>陽明交大管院院務學生代表</li>
+                <li>ATCC全國商業競賽 複賽決選</li>
+            </ul>
+        )
+    },{
+        title: '修課',
+        id: '修課',
+        image: '/images/修課.png',
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>資工系：資料結構與物件導向、演算法概論、網路程式設計概論、網路科學與架構、資料庫系統概論、計算機系統管理、計算機組織、作業系統、基礎程式設計</li>
+                <li>工工系：計算機概論、程式設計、作業研究(一)、作業研究(二)、模擬學、基因演算法與管理實務</li>
+                <li>通識課：數學應用軟體、Python基礎程式設計</li>
+            </ul>
+        )
+    }
+]
+
+
+const AboutSection = () => {
+    const [tab, setTab] = useState('學歷')
+    const [isPending, startTransition] = useTransition()
+    const handleTabChange = (id) => {
+        startTransition(() => {
+            setTab(id)
+        })
+    }
+    const currentTabData = TAB_DATA.find((item) => item.id === tab);
+    const currentImage = currentTabData ? currentTabData.image : "/images/學歷.png"; 
+  return (
+    <section className='text-white'>
+        <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
+            <Image src={currentImage} width={600} height={500} />
+            <div className='mt-4 md:mt-0 text-left flex flex-col h-full'> 
+                <h2 className='text-4xl font-bold text-white mb-4'>關於我</h2>
+                <p className='text-base lg:text-lg'>
+                    國立陽明交通大學工工系大四生，輔系資工系。
+                    平均GPA 4.05/4.30, Last 60 GPA 4.15/4.30。
+                    曾擔任系學會會長、管院學生代表、三次書卷獎。
+                </p>
+                <div className='flex flex-row justify-start mt-8'>
+                    <TapButton selectTab={() => handleTabChange('學歷')} active={tab === '學歷'}>學歷</TapButton>
+                    <TapButton selectTab={() => handleTabChange('技能')} active={tab === '技能'}>技能</TapButton>
+                    <TapButton selectTab={() => handleTabChange('經驗')} active={tab === '經驗'}>經驗</TapButton>
+                    <TapButton selectTab={() => handleTabChange('修課')} active={tab === '修課'}>修課</TapButton>
+                </div>
+                <div className='mt-8'>
+                    {TAB_DATA.find((item) => item.id === tab).content}
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default AboutSection
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { TypeAnimation } from 'react-type-animation';
+import Link from 'next/link';
 
 const HeroSection = () => {
     return (
@@ -25,15 +26,19 @@ const HeroSection = () => {
                         />
                     </h1>
                     <p className="text-[#85abc6] text-base sm:text-lg mb-6 lg:text-xl">
-                        我是一位前端工程師，喜歡使用 React.js 開發網站。
+                        歡迎來到我的個人網站，目前應徵後端實習中！
                     </p>
                     <div>
                         <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#f6d365] to-[#fda085] hover:bg-slate-200 text-black">
                             關於我
                         </button>
-                        <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#8b89c7] to-[#4f8396] hover:bg-slate-800 text-white mt-3">
-                            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">下載履歷</span>
-                        </button>
+                        <Link href="/documents/Heng_Jui__Hsu_Curriculum_Vitae.pdf" download>
+                            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#8b89c7] to-[#4f8396] hover:bg-slate-800 text-white mt-3">
+                                <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                                    下載履歷
+                                </span>
+                            </button>
+                        </Link>
                     </div>
                 </div>
                 <div className="col-span-5 place-self-center mt-4 lg:mg-0">
